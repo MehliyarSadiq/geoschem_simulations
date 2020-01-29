90669e27154d6dc963a00afd6af35cca23b51c71
MehliyarSadiq <mehliyar@gmail.com>
2020-01-29 12:56:09 +0000

    new function: split masks

--- TagCO2/py_code/util/functions.py
+++ TagCO2/py_code/util/functions.py
@@ -3,6 +3,7 @@
         Authors: Mehliyar Sadiq
         History: 2019-11-15, added area_latlon
                  2020-01-29, added create_masks
+                 2020-01-29, split_masks
 """
 
 import numpy as np
@@ -29,11 +30,18 @@ def area_latlon(lat1, lon1, lat2, lon2):
 
 # Function #2
 # Create corresponding region/country masks for an input file
-def create_masks(input_file, input_file_type='xarray', var_name='emi_co2', mask_type='giorgi', output_format='xarray', output_path='.', figure_flag = False, figure_path='.'):
+def create_masks(input_file, 
+                 input_file_type='dataarray', 
+                 var_name='emi_co2', 
+                 mask_type='giorgi', 
+                 output_format='xarray', 
+                 output_path='.', 
+                 figure_flag = False, 
+                 figure_path='.'):
     """
     This function creates mask files according to the resolution of input file.
-    	input_file could be NetCDF file or xarray, needs to be specified in input_file_type, 
-	input_file_type could be 'netcdf' or 'xarray'
+    	input_file could be NetCDF file or xarray dataarray, needs to be specified in input_file_type, 
+	input_file_type could be 'netcdf' or 'dataarray'
         var_name is the name of the variable in the file, preferably a two dimensional variable (lonxlat)
 	mask_type could be 'giorgi', 'srex', 'countries_110', 'countries_50', 'us_states_50', 'us_states_10'
     	output_format could be 'netcdf' or 'xarray'
@@ -45,8 +53,8 @@ def create_masks(input_file, input_file_type='xarray', var_name='emi_co2', mask_
         ds = xr.open_dataset(input_file) # read in netcdf file
         dr = ds[var_name]
         #print(dr)
-    elif(input_file_type=='xarray'):
-        dr=input_file[var_name]
+    elif(input_file_type=='dataarray'):
+        dr=input_file
     
     # get resolution of input data
     resolution_input_lon = dr.coords['lon'][3] - dr.coords['lon'][2]
@@ -90,8 +98,75 @@ def create_masks(input_file, input_file_type='xarray', var_name='emi_co2', mask_
 #for mask_type in ['giorgi', 'srex', 'countries_110', 'countries_50', 'us_states_50', 'us_states_10']:
 #    create_masks(input_file='ODIAC_2015_1x1.nc', input_file_type='netcdf', mask_type=mask_type, output_format='netcdf', output_path='./nc', figure_path='./plots')
 
+def split_masks(input_file, 
+                input_file_type='dataarray',
+                output_format='netcdf', 
+                output_path='./MASKS', 
+                figure_flag = True, 
+                figure_path='./figures'):
+    """
+    This function splits a mask created by above function into separate mask files
+        input_file could be NetCDF file or xarray dataarray, needs to be specified in input_file_type, 
+        input_file_type could be 'netcdf' or 'dataarray'
+        output_format could be 'netcdf' or 'xarray'
+        output_path is directory to store output (if format is netcdf)
+        figure_flag, if True, make figures for each mask file and save to figure_path
+        figure_path is directory to store the output figures
+    """
+    
+    nm_masks = input_file.max().values # number of masks
+    
+    # open a target file that you want to map your masks to
+    fname = '/home/msadiq/Desktop/co2/data/emission/GC/SE_Asia_mask.generic.1x1.nc'
+    ds = xr.open_dataset(fname)
+    
+    lon_len = len(ds.lon)
+    lat_len = len(ds.lat)
+    time_len = len(ds.time)
+    
+    resolution_output_lon = ds.coords['lon'][3] - ds.coords['lon'][2]
+    resolution_output_lat = ds.coords['lat'][3] - ds.coords['lat'][2]
+    resolution_output = str(resolution_output_lon.values) + 'x' + str(resolution_output_lat.values)
+    
+    for count in np.arange(nm_masks)+1:
+        
+        target = xr.Dataset({"MASK": (("time", "lat", "lon"), np.zeros(lon_len*lat_len*time_len).reshape(time_len,lat_len,lon_len))},
+                            coords=ds.coords)
+        target = target.astype(dtype='float32')
+        
+        mask_TF = input_file == count # True or False map
+        target['MASK'][0,:,:] = mask_TF.where(True)
+        
+        target['MASK'].attrs = ds['MASK'].attrs
+        target['lon'].attrs = ds['lon'].attrs
+        target['lat'].attrs = ds['lat'].attrs
+        target['time'].attrs = ds['time'].attrs
+        
+        target.attrs = ds.attrs
+        target.attrs['comment'] = 'region masks, by m.sadiq 2020'
+        target.attrs['history'] = 'made by m.sadiq 2020'
+        
+        if(output_format == 'netcdf'):
+            name_file = 'MASK' + str(count.astype(int)) + '_' + resolution_output + '.nc'
+            target.to_netcdf(output_path + '/' + name_file)
+            print(name_file +' NetCDf file is saved to: ' + output_path)
+            
+        if(figure_flag == True):
+            # plot the last mask
+            fig = plt.figure(figsize=[8, 4])
 
+            proj=ccrs.PlateCarree()
+            ax = plt.subplot(111, projection=proj)
 
-
+            target['MASK'].plot(ax=ax, transform=ccrs.PlateCarree(), cbar_kwargs={'shrink': 0.8})
+            ax.set_title("MASK " + str(count.astype(int)))
+            ax.set_label(' ')
+            ax.coastlines()
+            
+            fig.savefig(figure_path + '/mask_' + str(count.astype(int)) + '_' + resolution_output + '.png', dpi=300)
+            
+            print(name_file + ' figure is saved to: ' + figure_path)
+            
+    return
 
 
